a040774516ebeab095ec65a23eee016f08106c5d
waka2ya <waka2ya@users.noreply.github.com>
2022-01-12 09:05:57 +0900

    build for mac
    build for mac

--- Plugins/ArduinoKit/Source/ArduinoKit/Private/rs232.c
+++ Plugins/ArduinoKit/Source/ArduinoKit/Private/rs232.c
@@ -98,30 +98,30 @@ int RS232_OpenComport(int comport_number, int baudrate, const char *mode)
                    break;
     case  230400 : baudr = B230400;
                    break;
-    case  460800 : baudr = B460800;
-                   break;
-    case  500000 : baudr = B500000;
-                   break;
-    case  576000 : baudr = B576000;
-                   break;
-    case  921600 : baudr = B921600;
-                   break;
-    case 1000000 : baudr = B1000000;
-                   break;
-    case 1152000 : baudr = B1152000;
-                   break;
-    case 1500000 : baudr = B1500000;
-                   break;
-    case 2000000 : baudr = B2000000;
-                   break;
-    case 2500000 : baudr = B2500000;
-                   break;
-    case 3000000 : baudr = B3000000;
-                   break;
-    case 3500000 : baudr = B3500000;
-                   break;
-    case 4000000 : baudr = B4000000;
-                   break;
+//    case  460800 : baudr = B460800;
+//                   break;
+//    case  500000 : baudr = B500000;
+//                   break;
+//    case  576000 : baudr = B576000;
+//                   break;
+//    case  921600 : baudr = B921600;
+//                   break;
+//    case 1000000 : baudr = B1000000;
+//                   break;
+//    case 1152000 : baudr = B1152000;
+//                   break;
+//    case 1500000 : baudr = B1500000;
+//                   break;
+//    case 2000000 : baudr = B2000000;
+//                   break;
+//    case 2500000 : baudr = B2500000;
+//                   break;
+//    case 3000000 : baudr = B3000000;
+//                   break;
+//    case 3500000 : baudr = B3500000;
+//                   break;
+//    case 4000000 : baudr = B4000000;
+//                   break;
     default      : printf("invalid baudrate\n");
                    return(1);
                    break;
@@ -806,8 +806,8 @@ int RS232_GetPortnr(const char *devname)
 //UE4 Helper Functions
 int RS232_OpenComportUE4(char * c, int baudrate)
 {
-    int baudr,
-    status;
+    // int baudr,
+    // status;
 
     int comport_number = UE4_PORT_INDEX;
     comports[comport_number] = c;
